4d68df35b233789520b8ffba0278dae7d417f29e
tod619 <trevorodwyer619@hotmail.com>
2023-06-25 21:46:06 +0100

    Replace placeholder recipent + sender names with name from list + my_name in main.py file

--- main.py
+++ main.py
@@ -3,10 +3,18 @@
 # Replace the [name] placeholder with the actual name.
 # Save the letters in the folder "ReadyToSend".
 PLACEHOLDER = "[name]"
+PLACEHOLDER_Name = "Angela"
+MY_NAME = "Trevor"
 
 with open("./Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
 
 with open("./Input/Letters/starting_letter.txt") as letter_file:
-    letter = letter_file.read()
-    print(letter)
+    letter_contents = letter_file.read()
+
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        letter_with_name = new_letter.replace(PLACEHOLDER_Name, MY_NAME)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", "w") as completted_letter:
+            completted_letter.write(letter_with_name)
